d18ddaa7c3cd4831ecb06411af0ec168e309f515
Harry Maclean <harrymaclean@gocardless.com>
2017-08-06 15:01:25 +0100

    Add tests, fix some bugs

--- src/Business.hs
+++ src/Business.hs
@@ -1,63 +1,91 @@
 module Business
     (
-      isBusinessDay
+      Config (..)
+    , isBusinessDay
     , datesBetween
+    , rollForward
+    , rollBackward
+    , nextBusinessDay
+    , previousBusinessDay
+    , addBusinessDays
+    , subtractBusinessDays
+    , businessDaysBetween
     ) where
 
 import Data.Dates
+  ( DateTime
+  , WeekDay
+  , DateInterval (Days)
+  , addInterval
+  , minusInterval
+  , dateWeekDay
+  , datesDifference
+  )
 
-isBusinessDay :: DateTime -> Bool
-isBusinessDay = undefined
--- isBusinessDay date
---   | dateWeekDay date `notElem` workingDays = false
---   | date `elem` holidays = false
---   | otherwise = true
+data Config = Config
+  { workingDays :: [WeekDay]
+  , holidays :: [DateTime]
+  }
+
+isBusinessDay :: Config -> DateTime -> Bool
+isBusinessDay config date
+  | dateWeekDay date `notElem` workingDays config = False
+  | date `elem` holidays config = False
+  | otherwise = True
   
 -- Roll forward to the next business day. If the date given is a business
 -- day, that day will be returned. If the day given is a holiday or
 -- non-working day, the next non-holiday working day will be returned.
-rollForward :: DateTime -> DateTime
-rollForward date
-  | isBusinessDay date = date
-  | otherwise = rollForward (addInterval date (Days 1))
+rollForward :: Config -> DateTime -> DateTime
+rollForward config date
+  | isBusinessDay config date = date
+  | otherwise = rollForward config (addInterval date (Days 1))
 
 -- Roll backward to the previous business day. If the date given is a
 -- business day, that day will be returned. If the day given is a holiday or
 -- non-working day, the previous non-holiday working day will be returned.
-rollBackward :: DateTime -> DateTime
-rollBackward date
-  | isBusinessDay date = date
-  | otherwise = rollForward (minusInterval date (Days 1))
+rollBackward :: Config -> DateTime -> DateTime
+rollBackward config date
+  | isBusinessDay config date = date
+  | otherwise = rollBackward config (minusInterval date (Days 1))
 
 -- Roll forward to the next business day regardless of whether the given
 -- date is a business day or not.
-nextBusinessDay :: DateTime -> DateTime
-nextBusinessDay date = rollForward (addInterval date (Days 1))
+nextBusinessDay :: Config -> DateTime -> DateTime
+nextBusinessDay config date = rollForward config (addInterval date (Days 1))
 
-previousBusinessDay :: DateTime -> DateTime
-previousBusinessDay date = rollBackward (minusInterval date (Days 1))
+previousBusinessDay :: Config -> DateTime -> DateTime
+previousBusinessDay config date = rollBackward config (minusInterval date (Days 1))
 
 -- Add a number of business days to a date. If a non-business day is given,
 -- counting will start from the next business day. So,
 --   monday + 1 = tuesday
 --   friday + 1 = monday
 --   sunday + 1 = tuesday
-addBusinessDays :: DateTime -> DateInterval -> DateTime
-addBusinessDays date = addInterval (rollForward date)
+addBusinessDays :: Config -> DateTime -> Integer -> DateTime
+addBusinessDays config date 0 = rollForward config date
+addBusinessDays config date days
+  | days < 0 = subtractBusinessDays config date (abs days)
+  | otherwise = addBusinessDays config nextDate (days - 1)
+  where nextDate = addInterval (rollForward config date) (Days 1)
 
 -- Subtract a number of business days to a date. If a non-business day is
 -- given, counting will start from the previous business day. So,
 --   friday - 1 = thursday
 --   monday - 1 = friday
 --   sunday - 1 = thursday
-subtractBusinessDays :: DateTime -> DateInterval -> DateTime
-subtractBusinessDays date = minusInterval (rollBackward date)
+subtractBusinessDays :: Config -> DateTime -> Integer -> DateTime
+subtractBusinessDays config date 0 = rollBackward config date
+subtractBusinessDays config date days
+  | days < 0 = addBusinessDays config date (abs days)
+  | otherwise = subtractBusinessDays config prevDate (days - 1)
+  where prevDate = rollBackward config (minusInterval date (Days 1))
 
 -- Count the number of business days between two dates.
 -- This method counts from start of date1 to start of date2. So,
 -- business_days_between(mon, weds) = 2 (assuming no holidays)
-businessDaysBetween :: DateTime -> DateTime -> Int
-businessDaysBetween date1 date2 = length $ filter isBusinessDay (datesBetween date1 date2)
+businessDaysBetween :: Config -> DateTime -> DateTime -> Int
+businessDaysBetween config date1 date2 = length $ filter (isBusinessDay config) (datesBetween date1 date2)
 
 -- Returns the list of dates between the two given dates (inclusive of start)
 datesBetween :: DateTime -> DateTime -> [DateTime]
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,7 +5,132 @@ import Business
 main :: IO ()
 main = hspec $ do
   describe "datesBetween" $ do
-    it "returns the dates between the two given dates" $ do
-      datesBetween  date1 date2 `shouldBe` [date1, DateTime 2017 1 2 0 0 0]
-        where date1 = DateTime 2017 1 1 0 0 0
-              date2 = DateTime 2017 1 3 0 0 0
+    it "returns the dates between the two given dates" $
+      let date1 = DateTime 2017 1 1 0 0 0
+          date2 = DateTime 2017 1 3 0 0 0 in
+        datesBetween date1 date2 `shouldBe` [date1, DateTime 2017 1 2 0 0 0]
+    it "returns no dates when given the same date twice" $
+      let date = DateTime 2017 1 1 0 0 0 in
+        datesBetween date date `shouldBe` []
+    it "is inclusive of the start date, but exclusive of the end date" $
+      let start = DateTime 2017 1 1 0 0 0
+          end = DateTime 2017 1 2 0 0 0 in
+        datesBetween start end `shouldBe` [start]
+
+  describe "isBusinessDay" $
+    let monday = lastMonday $ DateTime 2017 1 1 0 0 0 in do
+      it "returns True when given a working day" $
+        let config = Config { workingDays = [Monday], holidays = [] } in
+          isBusinessDay config monday `shouldBe` True
+      it "returns False when given a non-working day" $
+        let config = Config { workingDays = [Tuesday], holidays = [] } in
+          isBusinessDay config monday `shouldBe` False
+      it "returns False when given a working day which is also a holiday" $
+        let config = Config { workingDays = [Monday], holidays = [monday] } in
+          isBusinessDay config monday `shouldBe` False
+      it "returns False when given a holiday which is a non-working day" $
+        let config = Config { workingDays = [Tuesday], holidays = [monday] } in
+          isBusinessDay config monday `shouldBe` False
+
+  describe "rollForward" $
+    let monday = lastMonday $ DateTime 2017 1 1 0 0 0
+        tuesday = addInterval monday (Days 1) in do
+      it "returns the given date if it is a working day" $
+        let config = Config { workingDays = [Monday], holidays = [] } in
+          rollForward config monday `shouldBe` monday
+      it "returns the next business day if the given date isn't one" $ do
+        let config = Config { workingDays = [Tuesday], holidays = [] } in
+          rollForward config monday `shouldBe` tuesday
+        let config = Config { workingDays = [Monday, Tuesday], holidays = [monday] } in
+          rollForward config monday `shouldBe` tuesday
+
+  describe "rollBackward" $
+    let monday = lastMonday $ DateTime 2017 1 1 0 0 0
+        tuesday = addInterval monday (Days 1)
+        wednesday = addInterval tuesday (Days 1) in do
+      it "returns the given date if it is a working day" $
+        let config = Config { workingDays = [Monday], holidays = [] } in
+          rollBackward config monday `shouldBe` monday
+      it "returns the previous business day if the given date isn't one" $ do
+        let config = Config { workingDays = [Monday], holidays = [] } in
+          rollBackward config tuesday `shouldBe` monday
+        let config = Config { workingDays = [Monday, Tuesday], holidays = [tuesday] } in
+          rollBackward config tuesday `shouldBe` monday
+        let config = Config { workingDays = [Monday, Tuesday], holidays = [tuesday] } in
+          rollBackward config wednesday `shouldBe` monday
+
+  describe "nextBusinessDay" $
+    let monday = lastMonday $ DateTime 2017 1 1 0 0 0
+        tuesday = addInterval monday (Days 1) in
+      it "returns the next business day from the given date, even if it is a business day" $ do
+        let config = Config { workingDays = [Monday, Tuesday], holidays = [] } in
+          nextBusinessDay config monday `shouldBe` tuesday
+        let config = Config { workingDays = [Tuesday], holidays = [] } in
+          nextBusinessDay config monday `shouldBe` tuesday
+        let config = Config { workingDays = [Monday, Tuesday], holidays = [monday] } in
+          nextBusinessDay config monday `shouldBe` tuesday
+
+  describe "previousBusinessDay" $
+    let monday = lastMonday $ DateTime 2017 1 1 0 0 0
+        tuesday = addInterval monday (Days 1) in
+      it "returns the previous business day from the given date, even if it is a business day" $ do
+        let config = Config { workingDays = [Monday, Tuesday], holidays = [] } in
+          previousBusinessDay config tuesday `shouldBe` monday
+        let config = Config { workingDays = [Monday], holidays = [] } in
+          previousBusinessDay config tuesday `shouldBe` monday
+        let config = Config { workingDays = [Monday, Tuesday], holidays = [tuesday] } in
+          previousBusinessDay config tuesday `shouldBe` monday
+
+  describe "addBusinessDays" $
+    let monday = lastMonday $ DateTime 2017 1 1 0 0 0
+        tuesday = addInterval monday (Days 1)
+        wednesday = addInterval tuesday (Days 1)
+        friday = addInterval wednesday (Days 2) in do
+      it "adds the given number of business days to the date" $
+        let config = Config { workingDays = [Monday, Tuesday, Wednesday], holidays = [] } in
+          addBusinessDays config monday 1 `shouldBe` tuesday
+      context "when there are non-working days or holidays in the interval" $
+        it "skips them" $ do
+          let config = Config { workingDays = [Monday, Wednesday], holidays = [] } in
+              addBusinessDays config monday 1 `shouldBe` wednesday
+          let config = Config { workingDays = [Monday, Tuesday, Wednesday, Friday], holidays = [wednesday] } in
+              addBusinessDays config monday 2 `shouldBe` friday
+      context "when given a negative number of days" $
+        it "delegates to subtractBusinessDays, and does the expected thing" $
+          let config = Config { workingDays = [Monday, Tuesday], holidays = [] } in
+              addBusinessDays config tuesday (-1) `shouldBe` monday
+
+  describe "subtractBusinessDays" $
+    let monday = lastMonday $ DateTime 2017 1 1 0 0 0
+        tuesday = addInterval monday (Days 1)
+        wednesday = addInterval tuesday (Days 1)
+        friday = addInterval wednesday (Days 2) in do
+      it "subtracts the given number of business days from the date" $
+        let config = Config { workingDays = [Monday, Tuesday, Wednesday], holidays = [] } in
+          subtractBusinessDays config tuesday 1 `shouldBe` monday
+      context "when there are non-working days or holidays in the interval" $
+        it "skips them" $ do
+          let config = Config { workingDays = [Monday, Wednesday], holidays = [] } in
+              subtractBusinessDays config wednesday 1 `shouldBe` monday
+          let config = Config { workingDays = [Monday, Tuesday, Wednesday, Friday], holidays = [wednesday] } in
+              subtractBusinessDays config friday 2 `shouldBe` monday
+      context "when given a negative number of days" $
+        it "delegates to addBusinessDays, and does the expected thing" $
+          let config = Config { workingDays = [Monday, Tuesday], holidays = [] } in
+              subtractBusinessDays config monday (-1) `shouldBe` tuesday
+  describe "businessDaysBetween" $
+    it "returns the number of business days between the two days" $
+      let monday = lastMonday $ DateTime 2017 1 1 0 0 0
+          tuesday = addInterval monday (Days 1)
+          wednesday = addInterval tuesday (Days 1)
+          thursday = addInterval wednesday (Days 1)
+          friday = addInterval thursday (Days 1) in do
+        let config = Config { workingDays = [Monday, Tuesday, Wednesday, Thursday, Friday], holidays = [] } in do
+            businessDaysBetween config monday friday `shouldBe` 4
+            businessDaysBetween config monday (nextMonday monday) `shouldBe` 5
+        let config = Config { workingDays = [Monday, Tuesday, Wednesday, Thursday, Friday], holidays = [thursday] }  in
+            businessDaysBetween config monday friday `shouldBe` 3
+        let config = Config { workingDays = [Monday, Tuesday, Wednesday, Thursday, Friday], holidays = [monday] }  in
+            businessDaysBetween config monday tuesday `shouldBe` 0
+        let config = Config { workingDays = [Monday, Wednesday, Thursday, Friday], holidays = [tuesday] } in
+          businessDaysBetween config monday friday `shouldBe` 3
